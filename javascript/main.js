0e2c4d197a6c4443212dc80aaca0ee162a8883a4
Renzo Tomlinson <rttomlinson@gmail.com>
2017-06-21 22:54:34 +0000

    Create presentational slide

--- javascript/main.js
+++ javascript/main.js
@@ -6,24 +6,21 @@ $( document ).ready(function() {
     //attached event listener to slider-next button
     $('.slider-next').click(function() {
         ++currentSlidePosition;
-        console.log("next currentSlidePosition", currentSlidePosition);
         //grab the .slider, pull the data value off of the item, multiply the translation
-        
         let $slides = $('.slides');
         $slides.css({transform: `translateX(${currentSlidePosition * -50}%)`});
-        
-        
     });
     $('.slider-prev').click(function() {
         --currentSlidePosition;
-        console.log("prev currentSlidePosition", currentSlidePosition);
         //grab the .slider, pull the data value off of the item, multiply the translation
-        
         let $slides = $('.slides');
         $slides.css({transform: `translateX(${currentSlidePosition * -50}%)`});
-        
-        
     });
+    
+    
+    function updatePresentationalSlide() {
+        
+    }
     
     
 });
