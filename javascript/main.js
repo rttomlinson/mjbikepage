81aac7017ec8be8189e91e7d2beb281d6b189da9
Renzo Tomlinson <rttomlinson@gmail.com>
2017-06-21 22:25:46 +0000

    next and prev slider functionality

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,29 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+    
+    
+    let currentSlidePosition = 0;
+    //attached event listener to slider-next button
+    $('.slider-next').click(function() {
+        ++currentSlidePosition;
+        console.log("next currentSlidePosition", currentSlidePosition);
+        //grab the .slider, pull the data value off of the item, multiply the translation
+        
+        let $slides = $('.slides');
+        $slides.css({transform: `translateX(${currentSlidePosition * -50}%)`});
+        
+        
+    });
+    $('.slider-prev').click(function() {
+        --currentSlidePosition;
+        console.log("prev currentSlidePosition", currentSlidePosition);
+        //grab the .slider, pull the data value off of the item, multiply the translation
+        
+        let $slides = $('.slides');
+        $slides.css({transform: `translateX(${currentSlidePosition * -50}%)`});
+        
+        
+    });
+    
+    
 });
