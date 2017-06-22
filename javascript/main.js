906e193b1bf74766eec5aa45341cc2e3f50a8412
Renzo Tomlinson <rttomlinson@gmail.com>
2017-06-22 04:11:17 +0000

    Add handlebars

--- javascript/main.js
+++ javascript/main.js
@@ -111,3 +111,10 @@ $(document).ready(function() {
         });
     }
 });
+
+
+function makeToPercents(arr) {
+    return arr.map((coord) => {
+        return [(960 / coord[0]) * 100, (700 / coord[1]) * 100]
+    })
+}
