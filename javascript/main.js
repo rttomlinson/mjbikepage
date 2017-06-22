279668fc303478008f9c3b222066df11dfbeb86f
Renzo Tomlinson <rttomlinson@gmail.com>
2017-06-22 00:21:33 +0000

    add transition methods

--- javascript/main.js
+++ javascript/main.js
@@ -1,26 +1,63 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-    
-    
-    let currentSlidePosition = 0;
-    //attached event listener to slider-next button
-    $('.slider-next').click(function() {
-        ++currentSlidePosition;
-        //grab the .slider, pull the data value off of the item, multiply the translation
-        let $slides = $('.slides');
-        $slides.css({transform: `translateX(${currentSlidePosition * -50}%)`});
-    });
-    $('.slider-prev').click(function() {
-        --currentSlidePosition;
-        //grab the .slider, pull the data value off of the item, multiply the translation
-        let $slides = $('.slides');
-        $slides.css({transform: `translateX(${currentSlidePosition * -50}%)`});
-    });
-    
-    
-    function updatePresentationalSlide() {
-        
-    }
-    
-    
-});
+$(document).ready(function() {
+            console.log("ready!");
+
+
+            let currentSlidePosition = 0;
+            //should pull this from the initial load
+            let currentSliderOffset = 25;
+            //attached event listener to slider-next button
+            $('.slider-next').click(function() {
+                fadeOutPresentationalSlide();
+                ++currentSlidePosition;
+                currentSliderOffset -= 50;
+                //grab the .slider, pull the data value off of the item, multiply the translation
+                let $slides = $('.slides');
+                $slides.css({
+                    transform: `translateX(${currentSliderOffset}%)`
+                });
+                fadeInPresentationalSlide();
+            });
+            $('.slider-prev').click(function() {
+                fadeOutPresentationalSlide();
+                --currentSlidePosition;
+                currentSliderOffset += 50;
+                //grab the .slider, pull the data value off of the item, multiply the translation
+                let $slides = $('.slides');
+                $slides.css({
+                    transform: `translateX(${currentSliderOffset}%)`
+                });
+                fadeInPresentationalSlide();
+            });
+
+
+            function fadeOutPresentationalSlide() {
+                console.log("fade out");
+                //fade out the current slide and reduce scale
+                return new Promise((resolve, reject) => {
+                        $(".presentational-slide").css({
+                            transform: "scale(1.0)"
+                        });
+                    });
+                    //wait until translation has finished
+                    //fade in and scale next slide
+                }
+
+                function fadeInPresentationalSlide() {
+                    console.log("fade in");
+                    //fade out the current slide and reduce scale
+                    $(".presentational-slide").css({
+                        transform: "scale(1.025)"
+                    });
+                    //wait until translation has finished
+                    //fade in and scale next slide
+                }
+
+                function slideTransition() {
+                    $(".myClass").one('transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd',
+                        function() {
+                            //do something
+                        });
+                }
+
+
+            });
